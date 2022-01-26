50fcdc81aefd1f06866457331412b10cdabcb81a
Dirk Ziegelmeier <dziegelmeier@de.pepperl-fuchs.com>
2022-01-26 07:55:58 +0100

    Work on generated statemachine

--- Statemachine Test/generated/main.cxx
+++ Statemachine Test/generated/main.cxx
@@ -1,13 +1,44 @@
-#include <cpp_event_framework/Signal.hxx>
 #include <cpp_event_framework/Statemachine.hxx>
+#include <iostream>
+
+enum class EEvent
+{
+    S_PNS_ArOpend_ind,
+    S_PNS_ParamEndInd,
+    S_PNS_ArClosed_ind
+};
 
 class IoArHandler;
-using IoArHandlerMainBase = cpp_event_framework::Statemachine<IoArHandler, const cpp_event_framework::Signal::SPtr&>;
+using IoArHandlerMainBase = cpp_event_framework::Statemachine<IoArHandler, EEvent>;
 
 #include "IoArHandlerMainDeclaration.hxx"
 
 class IoArHandler
 {
+public:
+    IoArHandler() = default;
+
+    void Init()
+    {
+        fsm_.on_handle_event_ = [](IoArHandlerMain::StatePtr state, IoArHandlerMain::Event event)
+        { std::cout << "State " << state->name_ << " handle event " << static_cast<int>(event) << std::endl; };
+        fsm_.on_state_entry_ = [](IoArHandlerMain::StatePtr state)
+        { std::cout << "Enter state" << state->name_ << std::endl; };
+        fsm_.on_state_exit_ = [](IoArHandlerMain::StatePtr state)
+        { std::cout << "Exit state" << state->name_ << std::endl; };
+
+        fsm_.Init(this, "IoArHandlerMain", IoArHandlerMain::kInitialState);
+        fsm_.Start();
+    }
+
+    void Run()
+    {
+        fsm_.React(EEvent::S_PNS_ArOpend_ind);
+        fsm_.React(EEvent::S_PNS_ParamEndInd);
+        fsm_.React(EEvent::S_PNS_ArClosed_ind);
+    }
+
+private:
 #include "IoArHandlerMainPrototypes.hxx"
 
     friend class IoArHandlerMain;
@@ -15,60 +46,79 @@ class IoArHandler
 
     void ArClosed(IoArHandlerMain::Event)
     {
+        std::cout << __FUNCTION__ << std::endl;
     }
     void PrimarySwitchover(IoArHandlerMain::Event)
     {
+        std::cout << __FUNCTION__ << std::endl;
     }
     void BackupSwitchover(IoArHandlerMain::Event)
     {
+        std::cout << __FUNCTION__ << std::endl;
     }
     void Read(IoArHandlerMain::Event)
     {
+        std::cout << __FUNCTION__ << std::endl;
     }
     void Write(IoArHandlerMain::Event)
     {
+        std::cout << __FUNCTION__ << std::endl;
     }
     void CheckModuleDiff(IoArHandlerMain::Event)
     {
+        std::cout << __FUNCTION__ << std::endl;
     }
     void ApplyConfiguration(IoArHandlerMain::Event)
     {
+        std::cout << __FUNCTION__ << std::endl;
     }
     void StartWaitApplicationReadyTimer(IoArHandlerMain::StatePtr)
     {
+        std::cout << __FUNCTION__ << std::endl;
     }
     void StartWaitApplicationReadyTimer2(IoArHandlerMain::Event)
     {
+        std::cout << __FUNCTION__ << std::endl;
     }
     void StopWaitApplicationReadyTimer(IoArHandlerMain::StatePtr)
     {
+        std::cout << __FUNCTION__ << std::endl;
     }
     void SendApplicationReady(IoArHandlerMain::StatePtr)
     {
+        std::cout << __FUNCTION__ << std::endl;
     }
     void AbortAr(IoArHandlerMain::Event)
     {
+        std::cout << __FUNCTION__ << std::endl;
     }
     void DrPull(IoArHandlerMain::Event)
     {
+        std::cout << __FUNCTION__ << std::endl;
     }
     void DrPlug(IoArHandlerMain::Event)
     {
+        std::cout << __FUNCTION__ << std::endl;
     }
     void StartDynReconfTimer(IoArHandlerMain::StatePtr)
     {
+        std::cout << __FUNCTION__ << std::endl;
     }
     void StopDynReconfTimer(IoArHandlerMain::StatePtr)
     {
+        std::cout << __FUNCTION__ << std::endl;
     }
     void DynReconfTimeoutAbortArSet(IoArHandlerMain::Event)
     {
+        std::cout << __FUNCTION__ << std::endl;
     }
     void DrPullDone(IoArHandlerMain::Event)
     {
+        std::cout << __FUNCTION__ << std::endl;
     }
     void DrPlugDone(IoArHandlerMain::Event)
     {
+        std::cout << __FUNCTION__ << std::endl;
     }
 };
 
@@ -78,4 +128,7 @@ class IoArHandler
 
 int main()
 {
+    IoArHandler h;
+    h.Init();
+    h.Run();
 }
