cd31c09274331dfec1016e28877b63444b76bb09
Dirk Ziegelmeier <dziegelmeier@de.pepperl-fuchs.com>
2022-01-26 07:07:43 +0100

    Work on statemachine generation

--- Statemachine Test/generated/main.cxx
+++ Statemachine Test/generated/main.cxx
@@ -13,15 +13,69 @@ class IoArHandler
     friend class IoArHandlerMain;
     IoArHandlerMain fsm_;
 
-    void ArClosed(IoArHandlerMain::Event event);
-    void PrimarySwitchover(IoArHandlerMain::Event event);
-    void BackupSwitchover(IoArHandlerMain::Event event);
-    void Read(IoArHandlerMain::Event event);
-    void Write(IoArHandlerMain::Event event);
-    void CheckModuleDiff(IoArHandlerMain::Event event);
-    void ApplyConfiguration(IoArHandlerMain::Event event);
-    void StartTimer(IoArHandlerMain::StatePtr event);
-    void StopTimer(IoArHandlerMain::StatePtr event);
+    void ArClosed(IoArHandlerMain::Event)
+    {
+    }
+    void PrimarySwitchover(IoArHandlerMain::Event)
+    {
+    }
+    void BackupSwitchover(IoArHandlerMain::Event)
+    {
+    }
+    void Read(IoArHandlerMain::Event)
+    {
+    }
+    void Write(IoArHandlerMain::Event)
+    {
+    }
+    void CheckModuleDiff(IoArHandlerMain::Event)
+    {
+    }
+    void ApplyConfiguration(IoArHandlerMain::Event)
+    {
+    }
+    void StartWaitApplicationReadyTimer(IoArHandlerMain::StatePtr)
+    {
+    }
+    void StartWaitApplicationReadyTimer2(IoArHandlerMain::Event)
+    {
+    }
+    void StopWaitApplicationReadyTimer(IoArHandlerMain::StatePtr)
+    {
+    }
+    void SendApplicationReady(IoArHandlerMain::StatePtr)
+    {
+    }
+    void AbortAr(IoArHandlerMain::Event)
+    {
+    }
+    void DrPull(IoArHandlerMain::Event)
+    {
+    }
+    void DrPlug(IoArHandlerMain::Event)
+    {
+    }
+    void StartDynReconfTimer(IoArHandlerMain::StatePtr)
+    {
+    }
+    void StopDynReconfTimer(IoArHandlerMain::StatePtr)
+    {
+    }
+    void DynReconfTimeoutAbortArSet(IoArHandlerMain::Event)
+    {
+    }
+    void DrPullDone(IoArHandlerMain::Event)
+    {
+    }
+    void DrPlugDone(IoArHandlerMain::Event)
+    {
+    }
 };
 
 #include "IoArHandlerMainInstance.hxx"
+
+#include "IoArHandlerMainHandlers.hxx"
+
+int main()
+{
+}
