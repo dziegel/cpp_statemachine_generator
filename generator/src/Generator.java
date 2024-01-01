808f843d8f4171a35de11290ce6d7baa0f7208be
Dirk Ziegelmeier <dirk@ziegelmeier.net>
2024-01-01 17:38:42 +0100

    Work on command line tool

--- generator/src/Generator.java
+++ generator/src/Generator.java
@@ -6,52 +6,53 @@ import org.eclipse.epsilon.emc.uml.UmlModel;
 import org.eclipse.epsilon.eol.models.IModel;
 
 public class Generator {
-  public static void main(String[] args) throws Exception {
-	if (args.length == 0) {
-	   System.err.println("Missing file argument.");
-	   return;
+	public static void main(String[] args) throws Exception {
+		if (args.length == 0) {
+			System.err.println("Missing file argument.");
+			return;
+		}
+
+		var factory = new EglFileGeneratingTemplateFactory();
+		factory.setTemplateRoot(
+				Generator.class.getProtectionDomain().getCodeSource().getLocation().toString());
+		var module = new EgxModule(factory);
+		module.parse(Generator.class.getResource("Program.egx"));
+
+		if (!module.getParseProblems().isEmpty()) {
+			System.err.println("Syntax errors found. Exiting.");
+			for (var prob : module.getParseProblems()) {
+				System.err.println(prob.toString());				
+			}
+			return;
+		}
+
+		IModel model = null;
+
+		var filename = args[0];
+		if (filename.endsWith(".uml") || filename.endsWith(".xmi")) {
+			// Load the XML document
+			var uml = new UmlModel();
+			uml.setModelFile(filename);
+			uml.setName("UML");
+			uml.load();
+			model = uml;
+
+			System.out.println("Generating XMI/UML model " + filename);
+		}
+
+		if (filename.endsWith(".scxml")) {
+			var scxml = new PlainXmlModel();
+			scxml.setFile(new File(filename));
+			scxml.setName("SCXML");
+			scxml.load();
+			model = scxml;
+
+			System.out.println("Generating SCXML model " + filename);
+		}
+
+		// Make the document visible to the EGX program
+		module.getContext().getModelRepository().addModel(model);
+		// ... and execute
+		module.execute();
 	}
-
-	var factory = new EglFileGeneratingTemplateFactory();
-	factory.setTemplateRoot(Generator.class.getProtectionDomain().getCodeSource().getLocation().toURI().toURL().toString());
-	factory.setTemplateRoot("/home/dziegel/event_framework/cpp_statemachine_generator/generator/src");
-    var module = new EgxModule(factory);
-    module.parse(new File("/home/dziegel/event_framework/cpp_statemachine_generator/generator/src/Program.egx").getAbsoluteFile());
-
-    if (!module.getParseProblems().isEmpty()) {
-      System.err.println("Syntax errors found. Exiting.");
-      return;
-    }
-
-    IModel model = null;
-    
-    var filename = args[0];
-    if (filename.endsWith(".uml") || filename.endsWith(".xmi"))
-    {
-        // Load the XML document
-        var uml = new UmlModel();
-        uml.setModelFile(args[0]);
-        uml.setName("UML");
-        uml.load();
-        model = uml;
-        
-        System.out.println("Generating XMI/UML model " + filename);
-    }
-    
-    if (filename.endsWith(".scxml"))
-    {
-        var scxml = new PlainXmlModel();
-        scxml.setFile(new File("library.xml"));
-        scxml.setName("SCXML");
-        scxml.load();
-        model = scxml;
-
-        System.out.println("Generating SCXML model " + filename);
 }
-
-    // Make the document visible to the EGX program
-    module.getContext().getModelRepository().addModel(model);
-    // ... and execute
-    module.execute();
-  }
-}  
